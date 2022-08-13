701ff7e1aff133ae43bba50476259c2956e86684
peng.1895 <85783400+wpf95@users.noreply.github.com>
2022-08-13 20:28:23 +0800

    Update genecards_func.py

--- genecards_func.py
+++ genecards_func.py
@@ -1,3 +1,8 @@
+##You need three python modules are re,requests,BeautifulSoup4.
+##Run the paogram and input your gene name split by space,that you can get the function in order.
+##It write by vision python 3.9.
+
+
 import re
 import requests
 from bs4 import BeautifulSoup
